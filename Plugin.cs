71a891f79cd82d493ef38a07337f83cbf5099d4c
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2026-01-20 18:20:48 +0100

    Ajoutez des fichiers projet.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using System;
+
+namespace LivingPlanetSystem
+{
+    [BepInPlugin(MyGuid, PluginName, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // Mod details
+        private const string MyGuid = "com.CaporalCross.LivingPlanetSystem";
+        private const string PluginName = "LivingPlanetSystem";
+        private const string Version = "1.0.0";
+
+        private void Awake()
+        {
+
+        }
+    }
+}
